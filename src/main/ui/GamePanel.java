90cd4183e14d3ad321f152718619fb8ffd702825
William Chow <wchow03@student.ubc.ca>
2022-03-22 16:05:21 -0700

    Replaced letters with Images for game

--- src/main/ui/GamePanel.java
+++ src/main/ui/GamePanel.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 public class GamePanel extends JPanel implements ActionListener {
     private Hand hand;
     private JButton[] buttons;
+    private ArrayList<ImageIcon> logos = new ArrayList<>();
     private ArrayList<Integer> selectedCards = new ArrayList<>();
     private ArrayList<Integer> matchedCards;
     private ArrayList<Integer> clearButtonLetter = new ArrayList<>();
@@ -19,8 +20,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
     // EFFECTS: creates a panel with cards * 2 buttons and no matched cards
     public GamePanel(int cards, Hand hand) {
-        this.hand = hand;
-        this.matchedCards = hand.getMatchedCards();
+        initializeHandAndLogos(hand);
 
         int row = cards / 2;
         int column = 4;
@@ -34,8 +34,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
     // EFFECTS: creates a panel with hand.getHandSize() buttons and cards that have been matched
     public GamePanel(Hand hand) {
-        this.hand = hand;
-        this.matchedCards = hand.getMatchedCards();
+        initializeHandAndLogos(hand);
 
         int row = hand.getHandSize() / 4;
         int column = 4;
@@ -46,10 +45,44 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     // MODIFIES: this
+    // EFFECTS: initializes hand, matched cards and logos
+    private void initializeHandAndLogos(Hand hand) {
+        this.hand = hand;
+        this.matchedCards = hand.getMatchedCards();
+        initializeLogos();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes logo images
+    private void initializeLogos() {
+        ImageIcon csharp = new ImageIcon("./data/C#Logo.png");
+        ImageIcon cplusplus = new ImageIcon("./data/C++Logo.png");
+        ImageIcon c = new ImageIcon("./data/CLogo.png");
+        ImageIcon css = new ImageIcon("./data/CSSLogo.png");
+        ImageIcon java = new ImageIcon("./data/JavaLogo.png");
+        ImageIcon js = new ImageIcon("./data/JSLogo.png");
+        ImageIcon python = new ImageIcon("./data/PythonLogo.png");
+        ImageIcon racket = new ImageIcon("./data/RacketLogo.png");
+        ImageIcon ruby = new ImageIcon("./data/RubyLogo.png");
+        ImageIcon swift = new ImageIcon("./data/SwiftLogo.png");
+        logos.add(csharp);
+        logos.add(cplusplus);
+        logos.add(c);
+        logos.add(css);
+        logos.add(java);
+        logos.add(js);
+        logos.add(python);
+        logos.add(racket);
+        logos.add(ruby);
+        logos.add(swift);
+    }
+
+    // MODIFIES: this
     // EFFECTS: creates handSize amount of buttons with questions marks
     private void createCardButtons(int handSize) {
         for (int i = 0; i < handSize; i++) {
             buttons[i] = new JButton("?");
+            buttons[i].setIcon(null);
             setUpButton(i);
         }
     }
@@ -60,7 +93,9 @@ public class GamePanel extends JPanel implements ActionListener {
         int matchedCardIndex = 0;
         for (int i = 0; i < hand.getHandSize(); i++) {
             if (matchedCardIndex < matchedCards.size() && matchedCards.get(matchedCardIndex) == i) {
-                buttons[i] = new JButton(String.valueOf(hand.getCardAt(i)));
+                buttons[i] = new JButton("");
+                placeImageOnButton(i);
+                placeImageOnButtonExtended(i);
                 matchedCardIndex++;
             } else {
                 buttons[i] = new JButton("?");
@@ -86,13 +121,12 @@ public class GamePanel extends JPanel implements ActionListener {
         for (int i = 0; i < hand.getHandSize(); i++) {
             if (e.getSource() == buttons[i]) {
                 if (clearButtonLetter.size() == 2 && !matchFound) {
-                    buttons[clearButtonLetter.get(0)].setText("?");
-                    buttons[clearButtonLetter.get(1)].setText("?");
-                    clearButtonLetter.clear();
+                    clearButtons();
                 }
                 if (!matchedCards.contains(i)) {
-                    buttons[i].setText(Character.toString(hand.getCardAt(i)));
-                    buttons[i].setBackground(Color.green);
+                    buttons[i].setText(null);
+                    placeImageOnButton(i);
+                    placeImageOnButtonExtended(i);
                     selectedCards.add(i);
 
                     if (selectedCards.size() == 2 && !matchedCards.contains(selectedCards.get(0))
@@ -105,6 +139,53 @@ public class GamePanel extends JPanel implements ActionListener {
                 }
             }
         }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: places image at corresponding char value
+    private void placeImageOnButton(int i) {
+        if (hand.getCardAt(i) == 'A') {
+            buttons[i].setIcon(resizeIcon(logos.get(0)));
+        }
+        if (hand.getCardAt(i) == 'B') {
+            buttons[i].setIcon(resizeIcon(logos.get(1)));
+        }
+        if (hand.getCardAt(i) == 'C') {
+            buttons[i].setIcon(resizeIcon(logos.get(2)));
+        }
+        if (hand.getCardAt(i) == 'D') {
+            buttons[i].setIcon(resizeIcon(logos.get(3)));
+        }
+        if (hand.getCardAt(i) == 'E') {
+            buttons[i].setIcon(resizeIcon(logos.get(4)));
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: places image at corresponding char value
+    private void placeImageOnButtonExtended(int i) {
+        if (hand.getCardAt(i) == 'F') {
+            buttons[i].setIcon(resizeIcon(logos.get(5)));
+        }
+        if (hand.getCardAt(i) == 'G') {
+            buttons[i].setIcon(resizeIcon(logos.get(6)));
+        }
+        if (hand.getCardAt(i) == 'H') {
+            buttons[i].setIcon(resizeIcon(logos.get(7)));
+        }
+        if (hand.getCardAt(i) == 'I') {
+            buttons[i].setIcon(resizeIcon(logos.get(8)));
+        }
+        if (hand.getCardAt(i) == 'J') {
+            buttons[i].setIcon(resizeIcon(logos.get(9)));
+        }
+    }
+
+    // EFFECTS: resizes icon to fit JButton
+    private ImageIcon resizeIcon(ImageIcon icon) {
+        Image img = icon.getImage();
+        Image resized = img.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
+        return new ImageIcon(resized);
     }
 
     // MODIFIES: this
@@ -121,6 +202,16 @@ public class GamePanel extends JPanel implements ActionListener {
             matchFound = false;
         }
         selectedCards.clear();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the buttons text back to "?"
+    private void clearButtons() {
+        buttons[clearButtonLetter.get(0)].setIcon(null);
+        buttons[clearButtonLetter.get(1)].setIcon(null);
+        buttons[clearButtonLetter.get(0)].setText("?");
+        buttons[clearButtonLetter.get(1)].setText("?");
+        clearButtonLetter.clear();
     }
 
     // EFFECTS: shows a pop up window if size of cards matched is same as hand size
